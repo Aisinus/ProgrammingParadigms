4144a41dd99c6c76619fbffd3389848de1af4bdc
aisen <xxxAuronblade277xxx@gmail.com>
2022-09-09 21:33:42 +0300

    final push

---
+++ ProblemSet2/Problem3.rkt
@@ -0,0 +1,20 @@
+#lang slideshow
+
+(define (dec n) (- n 1))
+
+(define (f n)
+  (cond
+    [(<= n 2) (- 10 n)]
+    [else (* (f(dec (dec n))) (f (dec n)))]
+    )
+  )
+
+(f 3)
+
+; = (* (f (dec (dec 3))) (f (dec 3)))
+; = (* (f (dec 2)) (f (dec 3)))
+; = (* (f 1) (f (dec 3)))
+; = (* 9 (f (dec 3)))
+; = (* 9 (f 2))
+; = (* 9 8)
+; = 72
